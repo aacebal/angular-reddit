8e5eef9a1bbfbce976a0b2489e8c2add889ba06c
Adel Acebal <adelacebal@yahoo.com>
2017-06-12 12:46:33 -0400

    upvotes and model working

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,28 +1,31 @@
 import { Component, OnInit, HostBinding } from '@angular/core';
+import { Article } from './article.model';
 
 @Component({
   selector: 'app-article',
   templateUrl: './article.component.html',
   styleUrls: ['./article.component.css']
 })
+
 export class ArticleComponent implements OnInit {
   @HostBinding('attr.class') cssClass = 'row';
-  votes: number;
-  title: string;
-  link: string;
+  article: Article;
 
   constructor() {
-  this.title = 'Angular 2';
-  this.link = 'http://angular.io';
-  this.votes = 10;
+    this.article = new Article(
+    'Angular 2',
+    'http://angular.io',
+    10);
+  }
+
+voteUp(): boolean {
+  this.article.votes += 1;
+  return false;
 }
 
-VoteUp() {
-  this.votes += 1;
-}
-
-voteDowne() {
-  this.votes -= 1;
+voteDown(): boolean {
+  this.article.votes -= 1;
+  return false;
 }
 
   ngOnInit() {
